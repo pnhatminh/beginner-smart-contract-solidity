b47d0ded89414bba15596e9acd30264a1c28ef9d
pnhatminh <p.nhatminh1502@gmail.com>
2022-06-11 16:24:57 +0700

    Add random generator from chainlink VRF

--- Lottery.sol
+++ Lottery.sol
@@ -1,16 +1,37 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.11;
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract Lottery {
+contract Lottery is VRFConsumerBase {
     address public owner;
     address payable[] public players;
     uint public lotteryId;
     mapping(uint => address payable) public lotteryWinningHistory;
 
-    constructor() {
-        owner = msg.sender;
-        lotteryId = 1;
+    bytes32 internal keyHash;
+    uint internal fee;
+    uint public randomResult;
+
+    constructor() 
+        VRFConsumerBase(
+            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF coordinator
+            0x01BE23585060835E02B77ef475b0Cc51aA1e0709 
+        ){
+            keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+            fee = 0.1 * 10 ** 18;
+            owner = msg.sender;
+            lotteryId = 1;
+    }
+
+    function getRandomNumber() public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK in contract");
+        return requestRandomness(keyHash, fee);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint randomness) internal override {
+        randomResult = randomness;
+        payWinner();
     }
 
     function getBalance() public view returns (uint) {
@@ -31,12 +52,12 @@ contract Lottery {
         players.push(payable(msg.sender));
     }
 
-    function getRandomNumber() public view returns (uint) {
-        return uint(keccak256(abi.encodePacked(owner, block.timestamp)));
+    function pickWinner() public onlyOwner {
+        getRandomNumber();
     }
 
-    function pickWinner() public onlyOwner { 
-        uint index = getRandomNumber() % players.length;
+    function payWinner() public  { 
+        uint index = randomResult % players.length;
         players[index].transfer(address(this).balance);
         lotteryWinningHistory[lotteryId] = players[index];
         lotteryId++;
